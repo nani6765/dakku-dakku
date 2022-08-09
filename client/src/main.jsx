468ec0e2ad5e26efbf9580c8e6c9ff8e677b024a
nani6765 <nani6765@gmail.com>
2022-08-09 14:58:44 +0900

    conflict

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -13,16 +12,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </RecoilRoot>
 );
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css';
-import {BrowserRouter as Router} from 'react-router-dom';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>
-)
->>>>>>> e1184c6820078bf3a9a019f8c13a252abd8a73bf
