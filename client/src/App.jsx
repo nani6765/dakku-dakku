7d8345330f4deb2fb60c7b663e0a152d570c7e5f
nani6765 <nani6765@gmail.com>
2022-08-09 14:48:40 +0900

    Jiyeon/dev

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import { Routes,Route,Navigate } from 'react-router-dom';
-import './App.css'
-import Login from "./components/login/Login.jsx"
-import Home from "./components/home/Home.jsx"
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/login/Login.jsx";
+import Home from "./components/home/Home.jsx";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path='/login' element={<Login/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { RecoilRoot } from 'recoil';
-import App from './App'
-import './index.css'
+import { RecoilRoot } from "recoil";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RecoilRoot>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </RecoilRoot>
-)
+);
