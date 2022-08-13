58e1b913973199298dc0c7b88d4d493866c9b475
nani6765 <nani6765@gmail.com>
2022-08-13 16:39:13 +0900

    MOVE : middleware separate (emailCheck)

--- server/router/user.js
+++ server/router/user.js
@@ -2,27 +2,9 @@ const router = require("express").Router();
 const { Counter } = require("../Model/counter.js");
 const { User } = require("../Model/user.js");
 
-const emailCheck = (req, res, next) => {
-  if (!req.body.email)
-    return res
-      .status(400)
-      .json({ success: false, msg: "필요한 항목의 값이 비어있습니다." });
+const middlewares = require("../util/middlewares.js");
 
-  User.find({ email: userData.email })
-    .exec()
-    .then((docs) => {
-      if (docs)
-        return res
-          .status(400)
-          .json({ success: false, msg: "중복된 email이 있습니다." });
-      else next();
-    })
-    .catch((err) => {
-      return res.status(400).json({ success: false, err });
-    });
-};
-
-router.post("/register", emailCheck, (req, res) => {
+router.post("/register", middlewares.emailCheck, (req, res) => {
   if (!req.body.uid || !req.body.displayName)
     return res
       .status(400)
