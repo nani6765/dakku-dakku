72be1d3e2193c238b056e233e51f272967ed8809
nani6765 <nani6765@gmail.com>
2022-08-13 15:21:55 +0900

    ADD : Create post.js

---
+++ server/router/post.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const { Counter } = require("../Model/counter.js");
+const { User } = require("../Model/user.js");
+const { Post } = require("../Model/post.js");
+
+module.exports = { router };
